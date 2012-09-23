6c7a6469cfe5c51e1febc9ce3e367375b3f3d888
pbressy <pbressy@gmail.com>
2012-09-23 17:36:18 +0100

    separation des css

---
+++ mod/winetheme/views/default/css/degusts/degusts.php
@@ -0,0 +1,313 @@
+<?php
+/*
+ * Winetheme css
+ */
+
+
+$couleurs_rouge = '/mod/winetheme/views/default/css/winetheme/images/couleurs_rouge.jpg';
+$couleurs_rouge = elgg_normalize_url($couleurs_rouge);
+
+$couleurs_blanc = '/mod/winetheme/views/default/css/winetheme/images/couleurs_blanc.jpg';
+$couleurs_blanc = elgg_normalize_url($couleurs_blanc);
+
+$couleurs_rose = '/mod/winetheme/views/default/css/winetheme/images/couleurs_rose.jpg';
+$couleurs_rose = elgg_normalize_url($couleurs_rose);
+
+$reflets_rouge = '/mod/winetheme/views/default/css/winetheme/images/reflets_rouge.png';
+$reflets_rouge = elgg_normalize_url($reflets_rouge);
+
+$reflets_blanc = '/mod/winetheme/views/default/css/winetheme/images/reflets_blanc.png';
+$reflets_blanc = elgg_normalize_url($reflets_blanc);
+
+$resto_background = '/mod/winetheme/views/default/css/winetheme/images/resto-50op.jpg';
+$resto_background = elgg_normalize_url($resto_background);
+
+$vigne_separator = '/mod/winetheme/views/default/css/winetheme/images/vigne.png';
+$vigne_separator = elgg_normalize_url($vigne_separator);
+
+$fiche = '/mod/winetheme/views/default/css/winetheme/images/fiche.png';
+$fiche = elgg_normalize_url($fiche);
+
+$degust_header = '/mod/winetheme/views/default/css/winetheme/images/degustation_header3.png';
+$degust_header = elgg_normalize_url($degust_header);
+
+
+$degust = '/mod/winetheme/views/default/css/winetheme/images/degust.jpg';
+$degust = elgg_normalize_url($degust);
+
+$help = '/mod/winetheme/views/default/css/winetheme/images/help.jpg';
+$help = elgg_normalize_url($help);
+
+?>
+
+
+
+#degust-fiche-left {
+padding-top: 10px;
+width: 630px;
+margin: 8px 0 5px 0px;
+background: #ffffff url(<?php echo $fiche; ?>) no-repeat;
+background-position: right top;
+
+border: 1px none #800000;
+
+}
+
+.degust-feuille-header {
+border-bottom: 1px none #800000;
+margin-bottom: 0px;
+padding-left: 5px;
+
+}
+
+.degust-feuille-header h1 {
+color: #000000;
+    font-weight: normal;
+font-size: 200%;
+border-bottom: 1px solid #800000;
+width: 60%;
+}
+
+.vigne_separator {
+clear:left;
+height: 33px;
+background: url(<?php echo $vigne_separator; ?>) no-repeat;
+background-position: center bottom;
+margin-bottom: 10px;
+}
+
+
+.degust-feuille-content {
+margin-bottom: 5px;
+min-height: 10px;
+display:block;
+}
+
+.degust-feuille-content-title {
+float:left;
+width: 200px;
+border: 1px none #590000;
+display:block;
+text-align: left;
+color: #800000;
+
+}
+
+.degust-feuille-content-value {
+float:left;
+width: 100px;
+display:block;
+text-align: left;
+}
+
+.degust-feuille-content-clear {
+clear:left;
+}
+
+#reflets_rouge {
+height:75px;
+background: url(<?php echo $reflets_rouge; ?>) no-repeat;
+background-position: center center;
+}
+
+#reflets_blanc {
+height:75px;
+background: url(<?php echo $reflets_blanc; ?>) no-repeat;
+background-position: center center;
+}
+
+#couleurs_rouge {
+height:75px;
+background: url(<?php echo $couleurs_rouge; ?>) no-repeat;
+background-position: center center;
+}
+
+#couleurs_blanc {
+height:75px;
+background: url(<?php echo $couleurs_blanc; ?>) no-repeat;
+background-position: center center;
+}
+
+#couleurs_rose {
+height:75px;
+background: url(<?php echo $couleurs_rose; ?>) no-repeat;
+background-position: center center;
+}
+
+
+.help_paragraph {
+        text-transform:none;
+}
+
+.help_paragraph h1 {
+        text-transform:none;
+}
+
+.help_picture img {
+display: block;
+margin-left:auto;
+margin-right:auto;
+}
+
+
+
+#vtab {
+    margin: auto;
+    width: 840px;
+    height: 100%;
+}
+#vtab > ul > li {
+    width: 60px;
+    height: 75px;
+    background-color: #fff !important;
+    list-style-type: none;
+    display: block;
+    text-align: center;
+    margin: auto;
+    padding-bottom: 10px;
+    border: 1px solid #fff;
+    position: relative;
+    border-right: none;
+    opacity: .3;
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=30)";
+    filter: progid:DXImageTransform.Microsoft.Alpha(Opacity=30);
+}
+
+
+
+#vtab > ul > li.home {
+    background: url(<?php echo $degust; ?>) no-repeat;
+}
+
+#vtab > ul > li.login {
+    background: url(<?php echo $help; ?>) no-repeat;
+}
+
+#vtab > ul > li.support {
+    background: url(<?php echo $help; ?>) no-repeat;
+
+}
+#vtab > ul > li.selected {
+    opacity: 1;
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+    filter: progid:DXImageTransform.Microsoft.Alpha(Opacity=100);
+    border: 1px solid #ddd;
+    border-right: none;
+    z-index: 10;
+    background-color: #ffffff !important;
+    position: relative;
+}
+#vtab > ul {
+    float: left;
+    width: 60px;
+    text-align: left;
+    display: block;
+    margin: auto 0;
+    padding: 0;
+    position: relative;
+    top: 30px;
+}
+#vtab > div {
+    background-color: #ffffff;
+    
+    margin-left: 60px;
+    border: 1px solid #ddd;
+    min-height: 500px;
+    padding: 12px;
+    position: relative;
+    z-index: 9;
+    -moz-border-radius: 20px;
+}
+#vtab > div > h4 {
+    color: #800;
+    font-weight: normal;
+    border-bottom: 1px solid #800000;
+    padding-top: 5px;
+    margin-top: 0;
+    margin-bottom: 10px;
+}
+
+
+
+
+div.error {
+	color: red;
+}
+
+div.error a {
+	color: #336699;
+	font-size: 12px;
+	text-decoration: underline
+}
+
+
+
+
+.legend_class {
+color: #800000;
+background: #ffffff;
+padding-left:10px;
+padding-right:10px;
+padding-top:5px;
+padding-bottom:5px;
+
+}
+
+
+
+
+.validate_error_label > label > h2 {
+color:#000000;
+text-align: center;
+    font-weight: normal;
+    margin-bottom: 5px;
+    margin-top: 5px;
+}
+
+
+
+.degust-main {
+
+margin-right: 0px;
+
+}
+
+.degust-side-head {
+padding-top:10px;
+height: 110px;
+text-transform:capitalize;  
+background: url(<?php echo $degust_header; ?>) no-repeat;
+background-position: right center;
+}
+
+
+
+.degust-sidebar {
+    float: right;
+    margin-bottom: 5px;
+    margin-left: 5px;
+    margin-right: 0px;
+    margin-top: 0px;
+    padding-bottom:10px;
+    padding-left: 0px;
+    padding-right: 0px;
+    padding-top: 0px;
+    position: relative;
+    border-width: 1px;
+    border-style: none;
+    width: 250px;
+}
+
+
+
+
+
+#list_user_degust {
+        width: 35%;
+        float: left;
+        
+}
+
+.elgg-col-2of3 {
+    width: 65%;
+}
---
+++ mod/winetheme/views/default/css/winetheme/winetheme.php
@@ -0,0 +1,215 @@
+<?php
+/*
+ * Winetheme css
+ */
+
+
+?>
+
+
+
+#avenue_activity ul.elgg-list.elgg-list-river.elgg-river > li {
+  display: none;
+  margin: 0 0 0px;
+  padding: 0 0 0px;
+  border: none;
+  border-bottom: 1px solid #e7e7e7;
+}
+
+#avenue_activity ul.elgg-list.elgg-list-river.elgg-river > li:last-child {
+  margin: 0;
+  padding: 0;
+  border: none;
+}
+
+
+
+
+/* ***************************************
+	Profile
+*************************************** */
+.profile {
+	float: left;
+	margin-bottom: 15px;
+}
+.profile .elgg-inner {
+	margin: 0 5px;
+	border: 2px solid #eee;
+	
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+}
+#profile-details {
+	padding: 15px;
+}
+/*** ownerblock ***/
+#profile-owner-block {
+	width: 200px;
+	float: left;
+	background-color: #eee;
+	padding: 15px;
+}
+#profile-owner-block .large {
+	margin-bottom: 10px;
+}
+#profile-owner-block a.elgg-button-action {
+	margin-bottom: 4px;
+	display: table;
+}
+.profile-content-menu a {
+	display: block;
+	
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+	
+	background-color: white;
+	margin: 3px 0 5px 0;
+	padding: 2px 4px 2px 8px;
+}
+.profile-content-menu a:hover {
+	background: #0054A7;
+	color: white;
+	text-decoration: none;
+}
+.profile-admin-menu {
+	display: none;
+}
+.profile-admin-menu-wrapper a {
+	display: block;
+	
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+	
+	background-color: white;
+	margin: 3px 0 5px 0;
+	padding: 2px 4px 2px 8px;
+}
+.profile-admin-menu-wrapper {
+	background-color: white;
+	
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+}
+.profile-admin-menu-wrapper li a {
+	background-color: white;
+	color: red;
+	margin-bottom: 0;
+}
+.profile-admin-menu-wrapper a:hover {
+	color: black;
+}
+/*** profile details ***/
+#profile-details .odd {
+	background-color: #f4f4f4;
+	
+	-webkit-border-radius: 0px; 
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+	
+	margin: 0 0 7px;
+	padding: 2px 4px;
+}
+#profile-details .even {
+	background-color:#f4f4f4;
+	
+	-webkit-border-radius: 0px; 
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+	
+	margin: 0 0 7px;
+	padding: 2px 4px;
+}
+.profile-aboutme-title {
+	background-color:#f4f4f4;
+	
+	-webkit-border-radius: 0px; 
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+	
+	margin: 0;
+	padding: 2px 4px;
+}
+.profile-aboutme-contents {
+	padding: 2px 0 0 3px;
+}
+.profile-banned-user {
+	border: 2px solid red;
+	padding: 4px 8px;
+	
+	-webkit-border-radius: 0px; 
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+}
+
+
+div#job{
+        display:none;
+}
+
+
+
+#list_user_degust {
+        width: 35%;
+        float: left;
+        
+}
+
+.elgg-col-2of3 {
+    width: 65%;
+}
+
+
+.groups-profile > .elgg-image {
+	margin-right: 10px;
+}
+
+.groups-stats {
+	background: #eeeeee;
+	padding: 5px;
+	margin-top: 10px;
+	
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+}
+
+.groups-profile-fields .odd,
+.groups-profile-fields .even {
+	background: #f4f4f4;
+	
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	border-radius: 0px;
+	
+	padding: 2px 4px;
+	margin-bottom: 7px;
+}
+
+.groups-profile-fields .elgg-output {
+	margin: 0;
+}
+
+#groups-tools > li {
+	width: 48%;
+	min-height: 200px;
+	margin-bottom: 40px;
+}
+
+#groups-tools > li:nth-child(odd) {
+	margin-right: 4%;
+}
+
+.groups-widget-viewall {
+	float: right;
+	font-size: 85%;
+}
+
+.groups-latest-reply {
+	float: right;
+}
+
+
