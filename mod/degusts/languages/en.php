e1eb6aacb25d5941c0a31e607b151811c5cfa40d
pbressy <pbressy@gmail.com>
2013-02-14 00:22:53 +0000

    traduction en anglais, et changement image

--- mod/degusts/languages/en.php
+++ mod/degusts/languages/en.php
@@ -11,15 +11,15 @@ $english = array(
   //Visuel   
     
     'degust:visuel' => "Visual analysis",
-    'degust:olfactif' => "olfactory analysis",
+    'degust:olfactif' => "Olfactory analysis",
     'degust:gustatif' => "Taste analysis",
     'degust:comment' => "Comments and notes",
     
     
-    'degust:couleur_intensity'=>'Intensity of the dress',             
+    'degust:couleur_intensity'=>'Intensity of the robe',             
     'degust:couleur'=>'The color',
     'degust:reflet'=>'The reflections',
-    'degust:bulle'=>'The bubble',
+    'degust:bulle'=>'The bubbles',
     
     
     'degust:couleur_intensity:pale' => 'Pale',
@@ -31,267 +31,267 @@ $english = array(
   
     'degust:couleur:grenat' => 'Garnet',
     'degust:couleur:carmin' => 'Carmine',
-    'degust:couleur:pourpre' => 'Pourpre',
-    'degust:couleur:rubis'=> 'Rubis',
-    'degust:couleur:tuile' => 'Tuile',
-    'degust:couleur:brun' => 'brun',
-    'degust:couleur:acajou' => 'acajou',
+    'degust:couleur:pourpre' => 'Purple',
+    'degust:couleur:rubis'=> 'Ruby',
+    'degust:couleur:tuile' => 'Tile',
+    'degust:couleur:brun' => 'Brown',
+    'degust:couleur:acajou' => 'Mahogany',
 
-    'degust:couleur:jaune_pale' => 'Jaune Pâle',
-    'degust:couleur:jaune' => 'Jaune',
-    'degust:couleur:paille' => 'Jaune paille',
-    'degust:couleur:or' => 'Or',
-    'degust:couleur:ambre' => 'Ambre',
+    'degust:couleur:jaune_pale' => 'Pale yellow',
+    'degust:couleur:jaune' => 'Yellow',
+    'degust:couleur:paille' => 'Straw',
+    'degust:couleur:or' => 'Gold',
+    'degust:couleur:ambre' => 'Amber',
   
-    'degust:couleur:rose_pale' => 'Rose Pâle',
-    'degust:couleur:rose' => 'Rose',
-    'degust:couleur:saumon'=> 'Saumon',
-    'degust:couleur:rubis' => 'Rubis',
-    'degust:couleur:oignon' => 'Oignon',
+    'degust:couleur:rose_pale' => 'Pale pink',
+    'degust:couleur:rose' => 'Pink',
+    'degust:couleur:saumon'=> 'Salmon',
+    'degust:couleur:rubis' => 'Ruby',
+    'degust:couleur:oignon' => 'Onion',
     'degust:couleur:orange' => 'Orange',
-    'degust:couleur:cuivre' => 'Cuivre',
+    'degust:couleur:cuivre' => 'Copper',
     
     
-    'degust:reflet:violace_jeune'=>'Violacé Jeune',
+    'degust:reflet:violace_jeune'=>'Pale purple',
     'degust:reflet:violine' => 'Violine',
-    'degust:reflet:grenat' => 'Grenat',
-    'degust:reflet:carmin' => 'Carmin',
-    'degust:reflet:brique' => 'Brique',
-    'degust:reflet:brun' => 'Brun',
+    'degust:reflet:grenat' => 'Garnet',
+    'degust:reflet:carmin' => 'Carmine',
+    'degust:reflet:brique' => 'Brick',
+    'degust:reflet:brun' => 'Brown',
     
-    'degust:reflet:vert' => 'Vert',
-    'degust:reflet:gris' => 'Gris',
-    'degust:reflet:or' => 'Or',
-    'degust:reflet:dore' => 'Doré',
+    'degust:reflet:vert' => 'Green',
+    'degust:reflet:gris' => 'Grey',
+    'degust:reflet:or' => 'Gold',
+    'degust:reflet:dore' => 'Golden',
     
     
-    'degust:mousse:inexistante'=>'Inexistante',
-    'degust:mousse:faible' => 'Faible',
-    'degust:mousse:abondante' => 'Abondante',
-    'degust:mousse:persistante' => 'Persistante',
+    'degust:mousse:inexistante'=>'Nonexistent',
+    'degust:mousse:faible' => 'Low',
+    'degust:mousse:abondante' => 'Abundant',
+    'degust:mousse:persistante' => 'Persistant',
 
-    'degust:bulle:tres_fine'=>'Très fines',
-    'degust:bulle:fine' => 'Fines',
-    'degust:bulle:moyenne' => 'Moyennes',
-    'degust:bulle:grossiere' => 'Grossières',
+    'degust:bulle:tres_fine'=>'Very fine',
+    'degust:bulle:fine' => 'Fine',
+    'degust:bulle:moyenne' => 'Medium',
+    'degust:bulle:grossiere' => 'Coarse',
 
     
              
 //Olfactif
-    'degust:nez_intensity'=>'L\'intensité',
-    'degust:nez'=>'Famille d\'arômes',    // résultat des checkboxes sous forme de tableau
-    'degust:nose'=>'Nez',
-    'degust:arome'=>'Arômes',        // description texte des arôme
-    'degust:complexity'=>'Complexité',   // en fonction du nombres de checkbox cochées.
+    'degust:nez_intensity'=>'The intensity',
+    'degust:nez'=>'Flavor family',    // résultat des checkboxes sous forme de tableau
+    'degust:nose'=>'Nose',
+    'degust:arome'=>'Flavors',        // description texte des arôme
+    'degust:complexity'=>'Complexity',   // en fonction du nombres de checkbox cochées.
     'degust:complexity:1'=>'Simple',
-    'degust:complexity:2'=>'Assez complexe',
+    'degust:complexity:2'=>'Complexe enough',
     'degust:complexity:3'=>'Complexe',
-    'degust:complexity:4'=>'Très complexe',
+    'degust:complexity:4'=>'Very complexe',
   
-    'degust:nez_intensity:ferme'=>'Fermé',
-    'degust:nez_intensity:discret' => 'Discret',
-    'degust:nez_intensity:ouvert' => 'Ouvert',
-    'degust:nez_intensity:expressif' => 'Expressif',
-    'degust:nez_intensity:exuberant' => 'Exubérant',
+    'degust:nez_intensity:ferme'=>'Closed',
+    'degust:nez_intensity:discret' => 'Discreet',
+    'degust:nez_intensity:ouvert' => 'Open',
+    'degust:nez_intensity:expressif' => 'Expressive',
+    'degust:nez_intensity:exuberant' => 'Exuberant',
                                 
 
 
-    'degust:nez:fruit'=>'Fruit',
+    'degust:nez:fruit'=>'Fruity',
     'degust:nez:floral' => 'Floral',
-    'degust:nez:vegetal' => 'Végétal',
-    'degust:nez:mineral' => 'Minéral',
-    'degust:nez:epice' => 'Epicé',
-    'degust:nez:fermentaire' => 'Fermentaire',
+    'degust:nez:vegetal' => 'Vegetal',
+    'degust:nez:mineral' => 'Mineral',
+    'degust:nez:epice' => 'Spicy',
+    'degust:nez:fermentaire' => 'Fermented',
     'degust:nez:animal' => 'Animal',
-    'degust:nez:empyreumatique' => 'Empyreumatique',
-    'degust:nez:bois' =>'Bois',
-    'degust:nez:chimique'=>'Chimique',
+    'degust:nez:empyreumatique' => 'Empyreumatic',
+    'degust:nez:bois' =>'Woody',
+    'degust:nez:chimique'=>'Chemical',
     
     //liste des arôme pour l'aide à la dégustation
-    'degust:nez_tips:fruit'=>'Pomme
-Pêche
-Poire
+    'degust:nez_tips:fruit'=>'Apple
+Peach
+Pear
 Orange
-Citron
-Abricot
-Fruits exotiques
-Amande
-Coing
-Noisette
-Noix
-Raisin
-Fruits rouges
-Fruits noirs',
+Lemon
+Apricot
+Exotic fruits
+Almond
+Quince
+Hazel
+Walnur
+Grape
+Red fruits
+Black fruits',
     
     'degust:nez_tips:floral' =>"Acacia 
-Fleur d'Oranger 
-Lilas 
-Genêt 
-Chèvrefeuille 
+Orange blossom 
+Lilac 
+Broom 
+Woodbine 
 Iris 
-Oeillets 
-fleurs blanches
-Fleurs séchées
+Carnations 
+White flowers
+Dried flowers
 Camomille
-Violette 
-Rose fanée 
-Fleurs séchées",
+Violet
+Wilted rose 
+Dried flowers",
     
-    'degust:nez_tips:vegetal' => 'Herbe fraîche 
-Paille 
-Lierre 
-Buis 
-Fougère
-Poivron 
+    'degust:nez_tips:vegetal' => 'Fresh grass 
+Straw 
+Ivy 
+Boxwood 
+Fern
+Pepper 
 Humus 
-Bourgeon de cassis 
-Champignon 
-Truffe 
-Sous-bois',
+Blackcurrent bud
+Mushroom 
+Truffle 
+Undergrowth',
     
-    'degust:nez_tips:mineral' => 'Pierre à fusil 
-Iode  
-Silex
-Craie
-Pétrole',
+    'degust:nez_tips:mineral' => 'Flint 
+Iodine  
+Flint
+Chalk
+Petrol',
     
     'degust:nez_tips:epice' => "Cake 
-Miel 
+Honey 
 Nougat 
-Pâte d'amande
-Cannelle 
-Poivre 
-Thym 
-Laurier
+Marzipan
+Cinnamon 
+Pepper 
+Thyme 
+Laurel
 Garrigue
-Réglisse 
+Licorice 
 Zan 
-Goudron",
+Tar",
     
     'degust:nez_tips:fermentaire' => 'Brioche 
-Mie de pain 
+Bread 
 Biscuit
-Levure
-Beurre
-Lait
-Yaourt',
+Yeast
+Butter
+Milk
+Yogurt',
     
-    'degust:nez_tips:animal' => 'Cuir 
-fourrure 
-Gibier 
-Venaison 
-Jus de viande',
+    'degust:nez_tips:animal' => 'Leather 
+Fur 
+Game 
+Venison 
+Meat juice',
     
-    'degust:nez_tips:empyreumatique' => "Cacao 
-Pain grillé 
-Pain d'épice 
-Tabac 
-Café 
+    'degust:nez_tips:empyreumatique' => "Cocoa 
+Toasted bread
+Gingerbread
+Tobbaco 
+Coffee 
 Caramel",
     
     'degust:nez_tips:bois' =>"Blasa 
-Pin 
-Cèdre 
-Résine de pin 
-Vanille
-Chêne
+Pine 
+Cedar
+Rosin
+Vanilla
+Oak
 Eucalyptus 
-Bois fumé 
-Bois brûlé 
-Bois neuf",
+Smoked wood
+Burned wood
+New wood",
     
-    'degust:nez_tips:chimique'=>'Solvant 
-Vernis 
-Peinture',
+    'degust:nez_tips:chimique'=>'Solvent 
+Varnish 
+Paint',
     
 
 //gustatif
 
-    'degust:rondeur:fluide'=>'Fluide',
-    'degust:rondeur:souple' => 'Souple',
-    'degust:rondeur:rond' => 'Ronde',
-    'degust:rondeur:plein'=> 'Pleine',
-    'degust:rondeur:volumineux' => 'Volumineuse',
-    'degust:rondeur:opulent' => 'Opulente',
-    'degust:rondeur:franche' => 'Franche',
-    'degust:rondeur:discrete' => 'Discrète',
+    'degust:rondeur:fluide'=>'Fluid',
+    'degust:rondeur:souple' => 'Flexible',
+    'degust:rondeur:rond' => 'Round',
+    'degust:rondeur:plein'=> 'Full',
+    'degust:rondeur:volumineux' => 'Large',
+    'degust:rondeur:opulent' => 'Opulent',
+    'degust:rondeur:franche' => 'Frank',
+    'degust:rondeur:discrete' => 'Discreet',
     
     
-    'degust:douceur:tendre'=>'Tendre',
-    'degust:douceur:suave' => 'Suave',
-    'degust:douceur:onctueux' => 'Onctueuse',
-    'degust:douceur:sirupeux' => 'Sirupeuse',
-    'degust:douceur:lourd' => 'Lourde',
+    'degust:douceur:tendre'=>'Tender',
+    'degust:douceur:suave' => 'Sweet',
+    'degust:douceur:onctueux' => 'Smooth',
+    'degust:douceur:sirupeux' => 'Syrupy',
+    'degust:douceur:lourd' => 'Heavy',
      
-    'degust:acidity:pateux'=>'Pateuse',
-    'degust:acidity:mou'=>'Molle',
-    'degust:acidity:frais' => 'Fraiche',
-    'degust:acidity:droit' => 'Droite',
-    'degust:acidity:nerveux' => 'Nerveuse',
-    'degust:acidity:vert' => 'Verte',
-    'degust:acidity:discrete'=>'Discrète',
-    'degust:acidity:elegante' => 'Elégante',
-    'degust:acidity:vif' => 'Vive',
+    'degust:acidity:pateux'=>'Pasty',
+    'degust:acidity:mou'=>'Soft',
+    'degust:acidity:frais' => 'Fresh',
+    'degust:acidity:droit' => 'Straight',
+    'degust:acidity:nerveux' => 'Nervous',
+    'degust:acidity:vert' => 'Green',
+    'degust:acidity:discrete'=>'Discreet',
+    'degust:acidity:elegante' => 'Elegant',
+    'degust:acidity:vif' => 'Lively',
     
-    'degust:alcool:leger'=>'Léger',
-    'degust:alcool:integre' => 'Intégré',
-    'degust:alcool:genereux' => 'Généreux',
-    'degust:alcool:chaud' => 'Chaud',
-    'degust:alcool:brulant' => 'Brûlant',
+    'degust:alcool:leger'=>'Light',
+    'degust:alcool:integre' => 'Integrated',
+    'degust:alcool:genereux' => 'Generous',
+    'degust:alcool:chaud' => 'Hot',
+    'degust:alcool:brulant' => 'Scalding',
                                              
-    'degust:tanin:souple'=>'Souples',
-    'degust:tanin:fondu' => 'Fondus',
-    'degust:tanin:soyeux' => 'Soyeux',
-    'degust:tanin:serre' => 'Serrés',
-    'degust:tanin:rustique' => 'Rustiques',
-    'degust:tanin:astringent' => 'Astringents',
+    'degust:tanin:souple'=>'Flexible',
+    'degust:tanin:fondu' => 'Melted',
+    'degust:tanin:soyeux' => 'Silky',
+    'degust:tanin:serre' => 'Tight',
+    'degust:tanin:rustique' => 'Rustic',
+    'degust:tanin:astringent' => 'Astringent',
     
 
-    'degust:longueur:tres_court'=>'Très courte',
-    'degust:longueur:court' => 'Courte',
-    'degust:longueur:moyen' => 'Moyenne',
-    'degust:longueur:long' => 'Longue',
-    'degust:longueur:tres_long' => 'Très Longue',
+    'degust:longueur:tres_court'=>'Very short',
+    'degust:longueur:court' => 'Short',
+    'degust:longueur:moyen' => 'Medium',
+    'degust:longueur:long' => 'Long',
+    'degust:longueur:tres_long' => 'Very long',
     
-    'degust:equilibre:parfait'=>'parfait',
-    'degust:equilibre:harmonieux' => 'harmonieux',
-    'degust:equilibre:equilibre' => 'Equilibré',
-    'degust:equilibre:desequilibre' => 'Déséquilibré',
+    'degust:equilibre:parfait'=>'Perfect',
+    'degust:equilibre:harmonieux' => 'Harmonious',
+    'degust:equilibre:equilibre' => 'Balanced',
+    'degust:equilibre:desequilibre' => 'Unbalanced',
     
 
 
-    'degust:palet_bulle:tres_fine'=>'Très fines',
-    'degust:palet_bulle:fine' => 'Fines',
-    'degust:palet_bulle:moyenne' => 'Moyennes',
-    'degust:palet_bulle:grossiere' => 'Grossières',
+    'degust:palet_bulle:tres_fine'=>'Very fine',
+    'degust:palet_bulle:fine' => 'Fine',
+    'degust:palet_bulle:moyenne' => 'Medium',
+    'degust:palet_bulle:grossiere' => 'Coarse',
     
     
-    'degust:rondeur'=>'Attaque et rondeur',
-    'degust:douceur'=>'La douceur',
-    'degust:acidity'=>'L\'acidité',
-    'degust:alcool'=>'L\'alcool',
-    'degust:tanin'=>'Texture et tannins',
-    'degust:moelleux'=>'Le moelleux',
+    'degust:rondeur'=>'Attach and roundness',
+    'degust:douceur'=>'The smoothness',
+    'degust:acidity'=>'The acidity',
+    'degust:alcool'=>'The alcohol',
+    'degust:tanin'=>'Texture and tannins',
+    'degust:moelleux'=>'The softness',
     'degust:retro'=>'Retro olfaction',
-    'degust:longueur'=>'Longueur en bouche',
-    'degust:bulle'=>'Les bulles',
-    'degust:mousse'=>'La mousse',
-    'degust:palet_bulle'=>'Les bulles en bouche',
-    'degust:equilibre' => 'Equilibre',
+    'degust:longueur'=>'Aftertaste',
+    'degust:bulle'=>'The bubbles',
+    'degust:mousse'=>'The foam',
+    'degust:palet_bulle'=>'The bubbles in the mouth',
+    'degust:equilibre' => 'Equilibrium',
  
  //Commentaire final
-    'degust:evolution'=>'L\'évolution',
-    'degust:comment'=>'Commentaire final',   //commentaire finale
-    'degust:context'=>'Contexte de dégustation (endroit, plat etc..)',        //proposition accord mets vins
-    'degust:note'=>'Note générale',
+    'degust:evolution'=>'The evolution',
+    'degust:comment'=>'Final comments',   //commentaire finale
+    'degust:context'=>'Wine tasting context (place, dish etc..)',        //proposition accord mets vins
+    'degust:note'=>'Overall rating',
     //comment
                     
-    'degust:evolution:tres_jeune'=>'Très Jeune',
-    'degust:evolution:jeune' => 'Jeune',
-    'degust:evolution:amorce' => 'Amorce',
+    'degust:evolution:tres_jeune'=>'Very young',
+    'degust:evolution:jeune' => 'Young',
+    'degust:evolution:amorce' => 'Priming',
     'degust:evolution:mature' => 'Mature',
-    'degust:evolution:evolue' => 'Evolué',
-    'degust:evolution:use' => 'Usé',
+    'degust:evolution:evolue' => 'Evolved',
+    'degust:evolution:use' => 'Used',
     
     'degust:price'=>'price',
     
