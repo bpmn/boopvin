612f781eac38f45eee23b3a5a80326d99a41770f
Fatxi <rety.fabien@gmail.com>
2013-02-19 21:10:10 +0100

    notif update news restobar

---
+++ add_notif.php
@@ -0,0 +1,10 @@
+<?php
+require_once(dirname(__FILE__) . "/engine/start.php");
+$users=elgg_get_entities(array('type'=>'user','limit'=>100));
+foreach ($users as $user) {
+	echo $user->name.'</br>' ;
+	echo set_user_notification_setting($user->getGUID(), 'site', true);
+	}
+
+
+?>
